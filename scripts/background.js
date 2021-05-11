8dcc8863f6c749b339afd89bbe138ebbf80267dd
Garnet3106 <manage@gant.work>
2021-05-11 11:41:52 +0900

    fix: bug when reload a tab

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,27 @@
-chrome.tabs.onUpdated.addListener((tabID, info, tab) => {
-    if(info.status === 'complete') {
-        if(tab.url.startsWith('https://twitter.com/i/bookmarks')) {
-            chrome.tabs.executeScript(null, {
-                file: './scripts/bookmarks.js'
-            }, () => {});
-        }
-
-        if(tab.url.startsWith('https://twitter.com')) {
-            chrome.tabs.executeScript(null, {
-                file: './scripts/tl.js'
-            }, () => {});
-        }
-    }
+chrome.tabs.query({
+    lastFocusedWindow: true
+}, (tabs) => {
+    setTimeout(() => {
+        executeScripts(tabs[0].url);
+    }, 5000);
 });
+
+chrome.tabs.onUpdated.addListener((tabID, info, tab) => {
+    if(info.status === 'complete')
+        executeScripts(tab.url);
+});
+
+
+function executeScripts(url) {
+    if(url.startsWith('https://twitter.com/i/bookmarks')) {
+        chrome.tabs.executeScript(null, {
+            file: './scripts/bookmarks.js'
+        }, () => {});
+    }
+
+    if(url.startsWith('https://twitter.com')) {
+        chrome.tabs.executeScript(null, {
+            file: './scripts/tl.js'
+        }, () => {});
+    }
+}
